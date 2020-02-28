67abb14898ef5b22c9d68bcdc1fb3fb7cd1d2812
claeysg <claeysg@wwu.edu>
2020-02-28 11:34:05 -0800

    some work on timer

--- project/screens/TimerScreen.js
+++ project/screens/TimerScreen.js
@@ -1,10 +1,47 @@
 import React, {Component} from 'react';
-import {ImageBackground, Button, Platform, StyleSheet, Text, Alert, TouchableOpacity, View} from 'react-native';
+import {ImageBackground, Button, Platform, StyleSheet, Text, Alert, TouchableOpacity, View, AppRegistry, TouchableHighlight} from 'react-native';
 import {NavigationContext} from '@react-navigation/native'
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer'
 
 
 export default class TimerScreen extends Component {
     static contextType = NavigationContext;
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        timerStart: false,
+        stopwatchStart: false,
+        totalDuration: 90000,
+        timerReset: false,
+        stopwatchReset: false,
+        currentTime: 0,
+      };
+      this.toggleTimer = this.toggleTimer.bind(this);
+      this.resetTimer = this.resetTimer.bind(this);
+      this.toggleStopwatch = this.toggleStopwatch.bind(this);
+      this.resetStopwatch = this.resetStopwatch.bind(this);
+    }
+ 
+    toggleTimer() {
+      this.setState({timerStart: !this.state.timerStart, timerReset: false});
+    }
+ 
+    resetTimer() {
+      this.setState({timerStart: false, timerReset: true});
+    }
+ 
+    toggleStopwatch() {
+      this.setState({stopwatchStart: !this.state.stopwatchStart, stopwatchReset: false});
+    }
+ 
+    resetStopwatch() {
+      this.setState({stopwatchStart: false, stopwatchReset: true});
+    }
+  
+    getFormattedTime(time) {
+        this.currentTime = time;
+    }
 
     render() {
         const navigation = this.context;
@@ -13,6 +50,38 @@ export default class TimerScreen extends Component {
                 <View style={styles.titleCircle}>
                     <Text style={styles.titleText}>Timer Running</Text>
                 </View>
+
+                <View >
+                <Stopwatch laps msecs start={this.state.stopwatchStart}
+                    reset={this.state.stopwatchReset}
+                    options={options}
+                    getTime={this.getFormattedTime} />
+
+                  <TouchableHighlight onPress={this.toggleStopwatch}>
+                    <Text style={{fontSize: 30}}>{!this.state.stopwatchStart ? "Start" : "Stop"}</Text>
+                  </TouchableHighlight>
+
+                  <TouchableHighlight onPress={this.resetStopwatch}>
+                    <Text style={{fontSize: 30}}>Reset</Text>
+                  </TouchableHighlight>
+
+                  <Timer totalDuration={this.state.totalDuration} msecs start={this.state.timerStart}
+                    reset={this.state.timerReset}
+                    options={options}
+                    handleFinish={handleTimerComplete}
+                    getTime={this.getFormattedTime} />
+
+                  <TouchableHighlight onPress={this.toggleTimer}>
+                    <Text style={{fontSize: 30}}>{!this.state.timerStart ? "Start" : "Stop"}</Text>
+                  </TouchableHighlight>
+
+                  <TouchableHighlight onPress={this.resetTimer}>
+                    <Text style={{fontSize: 30}}>Reset</Text>
+                  </TouchableHighlight>
+                </View>
+
+
+
                 <TouchableOpacity
                     style={styles.getStartedButton}
                     onPress={() => navigation.navigate('OverviewScreen')}
@@ -25,6 +94,23 @@ export default class TimerScreen extends Component {
     }
 
 }
+
+const handleTimerComplete = () => alert("custom completion function");
+ 
+const options = {
+  container: {
+    backgroundColor: '#000',
+    padding: 5,
+    borderRadius: 5,
+    width: 220,
+  },
+  text: {
+    fontSize: 30,
+    color: '#FFF',
+    marginLeft: 7,
+  }
+};
+ 
 
 const styles = StyleSheet.create({
   container: {
@@ -175,3 +261,5 @@ const styles = StyleSheet.create({
   },
 });
 
+
+AppRegistry.registerComponent('TimerScreen', () => TimerScreen);
